48ff8d79c21c527431dc1d7b8a36a4ebb6a877aa
Serdar Büyüktemiz <hserdarb@gmail.com>
2013-11-20 11:10:06 +0200

    git ignore file added

---
+++ src/test/App.Client.Web.Test/ConrollerTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Client.Web.Test
+{
+    [TestFixture]
+    public class ConrollerTests
+    {
+    }
+}
